6f46aa83a9985754d263a40d260b276c28779375
Laxynd <laxynd@siblaze.com>
2016-01-29 20:31:50 +0000

    Initial Commit

---
+++ Assets/Scripts/Entities/Entity.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Entity : MonoBehaviour {
+	
+	private float health, maxHealth;
+	
+	private abstract float getMaxHealth();
+	
+	public float getHealth(){
+		return health;
+	}
+	
+	public void addHealth(float amount){
+		health += amount;
+	}
+}
