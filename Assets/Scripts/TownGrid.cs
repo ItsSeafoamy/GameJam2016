20c4067bac00d7bffc56dbdc82cbb2951c929b1e
Laxynd <laxynd@siblaze.com>
2016-01-30 19:02:18 +0000

    Fixed placing stuff (p_-)

--- Assets/Scripts/TownGrid.cs
+++ Assets/Scripts/TownGrid.cs
@@ -7,7 +7,7 @@ public class TownGrid : MonoBehaviour {
     public float nodeRadius;
     public GameObject tilePrefab;
 
-    Node[,] grid;
+    Node[] grid;
 
     float nodeDiameter;
     int gridSizeX, gridSizeY;
@@ -27,30 +27,39 @@ public class TownGrid : MonoBehaviour {
     }
 
     void CreateGrid() {
-        grid = new Node[gridSizeX, gridSizeY];
+        grid = new Node[gridSizeX * gridSizeY];
         Vector3 worldBottomLeft = transform.position - Vector3.right * gridWorldSize.x / 2 - Vector3.forward * gridWorldSize.y / 2;
 
         for (int x = 0; x < gridSizeX; x++) {
             for (int y = 0; y < gridSizeY; y++) {
                 Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
 
-                grid[x, y] = new Node(worldPoint, true, x, y);
                 GameObject tile = (GameObject)Instantiate(tilePrefab, worldPoint, Quaternion.identity);
                 tile.transform.parent = transform;
+                grid[x + y * gridSizeX] = tile.GetComponent<Node>();
             }
         }
     }
 
-    public Node NodeFromWorldPosition(Vector3 worldPosition) {
-        float percentX = (worldPosition.x + gridWorldSize.x / 2) / gridWorldSize.x;
-        float percentY = (worldPosition.z + gridWorldSize.y / 2) / gridWorldSize.y;
-        percentX = Mathf.Clamp01(percentX);
-        percentY = Mathf.Clamp01(percentY);
+    //public Node NodeFromWorldPosition(Collider collider) {
+    //    //float percentX = (worldPosition.x + gridWorldSize.x / 2) / gridWorldSize.x;
+    //    //float percentY = (worldPosition.z + gridWorldSize.y / 2) / gridWorldSize.y;
+    //    //percentX = Mathf.Clamp01(percentX);
+    //    //percentY = Mathf.Clamp01(percentY);
 
-        int x = Mathf.RoundToInt((gridSizeX - 1) * percentX);
-        int y = Mathf.RoundToInt((gridSizeY - 1) * percentY);
-        return grid[x, y];
-    }
+    //    //int x = Mathf.RoundToInt((gridSizeX - 1) * percentX);
+    //    //int y = Mathf.RoundToInt((gridSizeY - 1) * percentY);
+
+    //    if (collider == null) return null;
+
+    //    foreach (Node n in grid) {
+    //        if (n.worldPosition.x == collider.transform.position.x && n.worldPosition.z == collider.transform.position.z) {
+    //            return n;
+    //        }
+    //    }
+
+    //    return null;
+    //}
 
     void OnDrawGizmos() {
         Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));
@@ -58,7 +67,7 @@ public class TownGrid : MonoBehaviour {
         if (grid != null) {
             foreach (Node n in grid) {
                 //Gizmos.color = (n.active) ? Color.blue : Color.red;
-                Gizmos.DrawCube(n.worldPosition, Vector3.one * 0.9f);
+                //Gizmos.DrawCube(n.worldPosition, Vector3.one * 0.9f);
             }
         }
     }
