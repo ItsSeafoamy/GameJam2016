cf68f0eb61910ecf97e4511d5fd597f4949cab1d
James Smith <james_smith.games@live.co.uk>
2016-01-30 13:54:48 +0000

    Added basic day progression, and placeholder ritual buttons

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -10,13 +10,32 @@ public class GameController : MonoBehaviour {
     public Transform directionalLight;
     public GameObject woodenTower, stoneTower, building;
     public GameObject enemy;
+    public GameObject ritualScreen;
+    public GameObject[] ritualButtons;
 
     //Attack happens on day 7
-    static int days;
+    static int day = 1;
 
     public Transform[] enemySpawns;
 
     public static GameObject placingObject;
+
+    void Start() {
+
+    }
+
+    void Update() {
+        //if (Input.GetMouseButton(1)) {
+        //    FadeOut(0);
+        //    FadeIn(1);
+        //}
+
+        if (Input.GetKeyDown(KeyCode.Q))
+            ShowRitualScreen();
+
+        if (Input.GetKeyDown(KeyCode.E))
+            HideRitualScreen();
+    }
 
     public void ChangeScene(string scene) {
         StartCoroutine(LoadingScreen.ChangeScene(scene));
@@ -39,11 +58,57 @@ public class GameController : MonoBehaviour {
     public void SpawnBuilding() {
         if (placingObject != null)
             Destroy(placingObject.gameObject);
+
         placingObject = (GameObject)Instantiate(building, Mouse.MousePosition, Quaternion.identity);
         text.text = "Building";
     }
 
-    public static void SpawnEnemies(int difficulty) {
+    public void ShowRitualScreen() {
+        ritualScreen.SetActive(true);
+    }
+
+    public void HideRitualScreen() {
+        ritualScreen.SetActive(false);
+    }
+
+    //void FadeIn(int i) {
+    //    if (audioChannels[i].volume < 1)
+    //        audioChannels[i].volume += 1 * Time.deltaTime;
+    //}
+
+    //void FadeOut(int i) {
+    //    if (audioChannels[i].volume > 1)
+    //        audioChannels[i].volume -= 1 * Time.deltaTime;
+    //}
+
+    public void HumanSacrifice() {
+        HideRitualScreen();
+    }
+
+    public void AnimalSacrifice() {
+        HideRitualScreen();
+    }
+    public void ResourceSacrifice() {
+        HideRitualScreen();
+    }
+    public void NoSacrifice() {
+        HideRitualScreen();
+    }
+
+
+    public void ProgressDay() {
+        if (day == 7) {
+            day = 1;
+        }
+        else
+            day++;
+
+        if (day == 7) {
+            ShowRitualScreen();
+            print("Run attack code here pls thank");
+        }
+
+        text.text = "Day: " + day;
 
     }
 }
