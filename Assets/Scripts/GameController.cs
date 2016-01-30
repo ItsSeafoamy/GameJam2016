824b9f6a0c8a131a683bc973086f8890b9b62aff
James Smith <james_smith.games@live.co.uk>
2016-01-30 21:08:16 +0000

    Added exploration code and icons in the textures folder.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -14,13 +14,16 @@ public class GameController : MonoBehaviour {
     public GameObject exploreBuildButton;
     public GameObject buildoptions;
     public GameObject[] ritualButtons;
+    public AudioSource audioSource;
+    public AudioClip[] musicTrack;
+    public Text explorationText;
+    public Exploration exploration;
 
     //Attack happens on day 7
     static int day = 1;
     public Transform[] enemySpawns;
     public static GameObject placingObject;
     public int woodCost, stoneCost;
-
 
     public void ChangeScene(string scene) {
         StartCoroutine(LoadingScreen.ChangeScene(scene));
@@ -30,44 +33,62 @@ public class GameController : MonoBehaviour {
         if (placingObject != null)
             Destroy(placingObject.gameObject);
             
-        if (Game.wood >= woodCost){
+        if (Game.wood >= 2){
         	placingObject = (GameObject)Instantiate(woodenTower, Mouse.MousePosition, Quaternion.identity);
         	text.text = "WoodenTower";
+            Game.wood -= 2;
         } else {
-        	text.text = "You need " + (woodCost-Game.wood) + " more wood to build this";
+        	text.text = "You need " + (2-Game.wood) + " more wood to build this";
         }
     }
     public void SpawnStoneTower() {
         if (placingObject != null)
             Destroy(placingObject.gameObject);
             
-        if (Game.stone >= stoneCost){
+        if (Game.stone >= 2){
        	 	placingObject = (GameObject)Instantiate(stoneTower, Mouse.MousePosition, Quaternion.identity);
         	text.text = "StoneTower";
+            Game.stone -= 2;
         } else {
-        	text.text = "You need " + (stoneCost-Game.stone) + " more stone to build this";
+        	text.text = "You need " + (2-Game.stone) + " more stone to build this";
         }
     }
     public void SpawnBuilding() {
         if (placingObject != null)
             Destroy(placingObject.gameObject);
-            
-        placingObject = (GameObject)Instantiate(building, Mouse.MousePosition, Quaternion.identity);
-        text.text = "Building";
+
+        if (Game.wood >= 3 && Game.stone >= 2) {
+            placingObject = (GameObject)Instantiate(building, Mouse.MousePosition, Quaternion.identity);
+            text.text = "Building";
+            Game.wood -= 3;
+            Game.stone -= 2;
+        }
+        else
+            text.text = "You need " + (2 - Game.stone) + " more stone and " + (3 - Game.wood) + " more wood to build this";
     }
     public void SpawnQuarry() {
         if (placingObject != null)
             Destroy(placingObject.gameObject);
-            
-        placingObject = (GameObject)Instantiate(quarry, Mouse.MousePosition, Quaternion.identity);
-        text.text = "Quarry";
+
+        if (Game.stone >= 10) {
+            placingObject = (GameObject)Instantiate(quarry, Mouse.MousePosition, Quaternion.identity);
+            text.text = "Quarry";
+            Game.stone -= 10;
+        }
+        else
+            text.text = "You need " + (10 - Game.stone) + " more stone to build this";
     }
     public void SpawnLogPost() {
         if (placingObject != null)
             Destroy(placingObject.gameObject);
-            
-        placingObject = (GameObject)Instantiate(logPost, Mouse.MousePosition, Quaternion.identity);
-        text.text = "Logging Post";
+
+        if (Game.wood >= 10) {
+            placingObject = (GameObject)Instantiate(logPost, Mouse.MousePosition, Quaternion.identity);
+            text.text = "Logging Post";
+            Game.wood -= 10;
+        }
+        else
+            text.text = "You need " + (10 - Game.wood) + " more wood to build this";
     }
 
     public void ShowRitualScreen() {
@@ -90,6 +111,36 @@ public class GameController : MonoBehaviour {
     }
     public void HideExplorationOptions() {
         exploreBuildButton.SetActive(false);
+    }
+
+    public void SwitchAudioTrack(int i) {
+        float timestamp = audioSource.time;
+        audioSource.clip = musicTrack[i];
+        audioSource.time = timestamp;
+        audioSource.Play();
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.E))
+            SwitchAudioTrack(1);
+        if (Input.GetKeyDown(KeyCode.Q))
+            SwitchAudioTrack(0);
+    }
+
+    public void Explore() {
+        explorationText.gameObject.SetActive(true);
+        explorationText.text = exploration.Explore();
+        StartCoroutine("Wait");
+    }
+
+    IEnumerator Wait() {
+
+
+        yield return new WaitForSeconds(3);
+
+
+        explorationText.gameObject.SetActive(false);
+        ProgressDay();
     }
 
     //void FadeIn(int i) {
@@ -129,6 +180,8 @@ public class GameController : MonoBehaviour {
         }
         else
             day++;
+
+        SwitchAudioTrack(day);
 
         if (day == 7) {
             ShowRitualScreen();
