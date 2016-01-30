0e33dc5e128809718d807c76cc941b48f8898d0c
Laxynd <laxynd@siblaze.com>
2016-01-30 01:21:10 +0000

    Villagers run away

--- Assets/Scripts/Entities/Villager.cs
+++ Assets/Scripts/Entities/Villager.cs
@@ -6,12 +6,27 @@ public class Villager : Human {
 	private Resource collecting;
 	
 	public void Update(){
+		NavMeshAgent nav = GetComponent<NavMeshAgent>();
+		Enemy nearestEnemy = getNearestEnemy();
+		
+		if (nearestEnemy != null){
+			if (Vector3.Distance(transform.position, nearestEnemy.transform.position) < 5f){
+				Vector3 dir = transform.position - nearestEnemy.transform.position;
+				dir = dir.normalized*10f;
+				
+				nav.SetDestination(transform.position + dir);
+				
+				if (collecting != null) collecting.beingHarvested = false;
+				return;
+			}
+		} 
+		
 		if (hasHarvestTool()){
 			if (collecting != null){
 				if (Vector3.Distance(transform.position, collecting.transform.position) < 2f){
 					collecting.beingHarvested = true;
 				} else {
-					GetComponent<NavMeshAgent>().SetDestination(collecting.transform.position);
+					nav.SetDestination(collecting.transform.position);
 					collecting.beingHarvested = false;
 				}
 			} else {
@@ -31,6 +46,25 @@ public class Villager : Human {
 				float dist = Vector3.Distance(resource.transform.position, transform.position);
 				if (dist < distance && resource.getPreferredTool() == item){
 					closest = resource;
+					distance = dist;
+				}
+			}
+			
+			return closest;
+		} else return null;
+	}
+	
+	private Enemy getNearestEnemy(){
+		Enemy[] enemies = Transform.FindObjectsOfType<Enemy>();
+		
+		if (enemies.Length != 0){
+			Enemy closest = null;
+			float distance = float.MaxValue;
+			
+			foreach (Enemy enemy in enemies){
+				float dist = Vector3.Distance(enemy.transform.position, transform.position);
+				if (dist < distance){
+					closest = enemy;
 					distance = dist;
 				}
 			}
